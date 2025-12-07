92cad272c61be143be66b3df5bebfd8585fb0464
Cesar Aguilar <Blueavian9@gmail.com>
2025-12-07 12:50:33 -0800

    Updated main.tsx

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/ui/Sidebar";
 import Topbar from "../../components/ui/Topbar";
 
